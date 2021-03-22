9a3c2a11297914796848602e96b7aa70bf6da516
fabricio-leitao <fabricio-beco@hotmail.com>
2021-03-22 18:56:19 -0300

    Problema de tipos diferentes

--- src/main/kotlin/Funcionario.kt
+++ src/main/kotlin/Funcionario.kt
@@ -1,9 +1,20 @@
 class Funcionario(
     var nome: String,
     var cpf: String,
-    var salario: Double
+    var salario: Double,
+    var tipo: Int //0- Funcionario, 1- Gerente, 2- Diretor
 ) {
     fun bonificacao(): Double {
-        return salario * 0.1;
+        when (tipo) {
+            0 -> {
+                return salario * 0.1
+            }
+            1 -> {
+                return salario * 0.2
+            }
+            else -> {
+                return salario * 0.3
+            }
+        }
     }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,7 +2,7 @@ fun main() {
     println("Bem vindo ao Bytebank!")
 
 
-    var todoroki = Funcionario("Todoroki", "111.222.333-44", 1000.0)
+    var todoroki = Funcionario("Todoroki", "111.222.333-44", 1000.0, 2)
 
     println("nome ${todoroki.nome}")
     println("cpf ${todoroki.cpf}")
