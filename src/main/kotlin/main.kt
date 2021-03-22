8fb26231eff78403aec71822d31768b2b0bdcc57
fabricio-leitao <fabricio-beco@hotmail.com>
2021-03-22 11:28:16 -0300

    Criação de classes e objetos

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,50 @@
 fun main() {
     println("Bem vindo ao Bytebank!")
 
-//    Ordem crescente
-//    for (i in 1..5){
-//        val titular: String = "Todoroki"
-//        val numeroConta: Int = i + 1000
-//        var saldo: Double = i + 10.0
-//
-//        println("titular: $titular")
-//        println("número da conta: $numeroConta")
-//        println("Saldo da conta: $saldo")
-//    }
+    val contaTodoroki = Conta()
+    contaTodoroki.titular = "Todoroki"
+    contaTodoroki.numeroConta = 123
+    contaTodoroki.saldo = 200.0
+
+    val contaDeku = Conta()
+    contaDeku.titular = "Deku"
+    contaDeku.numeroConta = 134
+    contaDeku.saldo = 300.0
+
+    println(contaTodoroki.titular)
+    println(contaTodoroki.numeroConta)
+    println(contaTodoroki.saldo)
+    println("--------------------")
+    println(contaDeku.titular)
+    println(contaDeku.numeroConta)
+    println(contaDeku.saldo)
+}
+
+class Conta(){
+    var titular = ""
+    var numeroConta = 0
+    var saldo = 0.0
+}
+
+fun testaCondicoes(saldo: Double) {
+    when {
+        saldo > 0.0 -> println("Saldo positivo!")
+        saldo == 0.0 -> println("Saldo neutro!")
+        else -> println("Saldo negativo!")
+    }
+}
+
+fun testaLacos(){
+//        Ordem crescente
+    for (i in 1..5){
+        val titular: String = "Todoroki"
+        val numeroConta: Int = i + 1000
+        var saldo: Double = i + 10.0
+
+        println("titular: $titular")
+        println("número da conta: $numeroConta")
+        println("Saldo da conta: $saldo")
+    }
 //      Ordem Decrescente
 //    for (i in 5 downTo 1){
 //        val titular: String = "Todoroki"
@@ -42,7 +76,7 @@ fun main() {
 //        println("Saldo da conta: $saldo")
 //    }
 
-    var i = 0
+//    var i = 0
 //    while(i < 5){
 //                val titular: String = "Todoroki"
 //        val numeroConta: Int = i + 1000
@@ -67,19 +101,11 @@ fun main() {
 
 
 //      Utilizando Break;
-    loop@ for (i in 1..100) {
-        println("i $i")
-        for (j in 1..100) {
-            println("j $j")
-            if (j == 5) break@loop
-        }
-    }
-}
-
-fun testaCondicoes(saldo: Double) {
-    when {
-        saldo > 0.0 -> println("Saldo positivo!")
-        saldo == 0.0 -> println("Saldo neutro!")
-        else -> println("Saldo negativo!")
-    }
+//    loop@ for (i in 1..100) {
+//        println("i $i")
+//        for (j in 1..100) {
+//            println("j $j")
+//            if (j == 5) break@loop
+//        }
+//    }
 }
