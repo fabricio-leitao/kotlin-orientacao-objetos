6b7b59e0286d0dd689c84a326b30a2a8336bf72a
fabricio-leitao <fabricio-beco@hotmail.com>
2021-03-22 09:41:15 -0300

    Variaveis com valores de int e double

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,14 @@
 fun main(){
     println("Bem vindo ao Bytebank!")
 
-    val titular = "Todoroki"
+    val titular: String = "Todoroki"
+    val numeroConta: Int = 1000
+    var saldo: Double = 0.0
+
+    saldo = 100.0
+    saldo += 200
 
     println("titular: $titular")
+    println("número da conta: $numeroConta")
+    println("Saldo da conta: $saldo")
 }
