b227e5324123a48bf8b31471c9d565d24f450ab3
fabricio-leitao <fabricio-beco@hotmail.com>
2021-03-22 18:38:42 -0300

    Classe Funcionario inserida

---
+++ src/main/kotlin/Funcionario.kt
@@ -0,0 +1,9 @@
+class Funcionario(
+    var nome: String,
+    var cpf: String,
+    var salario: Double
+) {
+    fun bonificacao(): Double {
+        return salario * 0.1;
+    }
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,13 @@
 fun main() {
     println("Bem vindo ao Bytebank!")
 
-    testaComportamentosConta()
+
+    var todoroki = Funcionario("Todoroki", "111.222.333-44", 1000.0)
+
+    println("nome ${todoroki.nome}")
+    println("cpf ${todoroki.cpf}")
+    println("salario ${todoroki.salario}")
+    println("Bonificação ${todoroki.bonificacao()}")
 }
 
 
