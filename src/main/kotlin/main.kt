96e39ed800f06901c48c9976d0e94c0a0a364981
fabricio-leitao <fabricio-beco@hotmail.com>
2021-03-22 14:51:42 -0300

    Encapsulando Getter e Setter

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,55 +4,55 @@ fun main() {
     val contaTodoroki = Conta()
     contaTodoroki.titular = "Todoroki"
     contaTodoroki.numeroConta = 123
-    contaTodoroki.saldo = 200.0
+    contaTodoroki.setSaldo(200.0)
 
     val contaDeku = Conta()
     contaDeku.titular = "Deku"
     contaDeku.numeroConta = 134
-    contaDeku.saldo = 300.0
+    contaDeku.setSaldo(300.0)
 
     println(contaTodoroki.titular)
     println(contaTodoroki.numeroConta)
-    println(contaTodoroki.saldo)
+    println(contaTodoroki.getSaldo())
     println("--------------------")
     println(contaDeku.titular)
     println(contaDeku.numeroConta)
-    println(contaDeku.saldo)
+    println(contaDeku.getSaldo())
 
-    println("---------------------------------")
-    println("Depositando na conta do Todoroki:")
-    contaTodoroki.deposita(50.0)
-    println(contaTodoroki.saldo)
-    println("Depositando na conta do Deku:")
-    contaDeku.deposita(70.0)
-    println(contaDeku.saldo)
-
-    println("---------------------------------")
-    println("Sacando da conta do Todoroki:")
-    contaTodoroki.saca(250.0)
-    println(contaTodoroki.saldo)
-    println("Sacando da conta do Deku:")
-    contaDeku.saca(200.0)
-    println(contaDeku.saldo)
-
-
-    println("----------------------------------")
-    println("Transferencia da conta do Deku para o Todoroki")
-
-    if(contaDeku.transfere(100.0, contaTodoroki)){
-        println("Transferência efetuada com sucesso!")
-    } else {
-        println("Falha na Transferência!")
-    }
-
-    println("Saldo Todoroki: ${contaTodoroki.saldo}")
-    println("Saldo Deku: ${contaDeku.saldo}")
+//    println("---------------------------------")
+//    println("Depositando na conta do Todoroki:")
+//    contaTodoroki.deposita(50.0)
+//    println(contaTodoroki.saldo)
+//    println("Depositando na conta do Deku:")
+//    contaDeku.deposita(70.0)
+//    println(contaDeku.saldo)
+//
+//    println("---------------------------------")
+//    println("Sacando da conta do Todoroki:")
+//    contaTodoroki.saca(250.0)
+//    println(contaTodoroki.saldo)
+//    println("Sacando da conta do Deku:")
+//    contaDeku.saca(200.0)
+//    println(contaDeku.saldo)
+//
+//
+//    println("----------------------------------")
+//    println("Transferencia da conta do Deku para o Todoroki")
+//
+//    if(contaDeku.transfere(100.0, contaTodoroki)){
+//        println("Transferência efetuada com sucesso!")
+//    } else {
+//        println("Falha na Transferência!")
+//    }
+//
+//    println("Saldo Todoroki: ${contaTodoroki.saldo}")
+//    println("Saldo Deku: ${contaDeku.saldo}")
 }
 
 class Conta() {
     var titular = ""
     var numeroConta = 0
-    var saldo = 0.0
+    private var saldo = 0.0
 
     fun deposita(valor: Double) {
         this.saldo += valor
@@ -71,6 +71,14 @@ class Conta() {
             return true
         }
             return false
+    }
+
+    fun getSaldo(): Double{
+        return saldo
+    }
+
+    fun setSaldo(saldo: Double){
+        this.saldo = saldo;
     }
 }
 
