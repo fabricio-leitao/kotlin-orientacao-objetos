059e2a88bd7dbebf1c3395a35e367210ed5f43d4
fabricio-leitao <fabricio-beco@hotmail.com>
2021-03-24 18:01:38 -0300

    Utilizando o Any no código

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,19 +4,25 @@ import br.com.zup.bytebank.teste.testaObjects
 fun main() {
 
     val endereco = Endereco()
+
     val objeto: Any = Any()
 
     imprime(objeto)
+    imprime(Unit)
+    imprime(Any())
     imprime(1)
     imprime(1.0)
     val teste: Any = imprime(endereco)
     println(teste)
+    imprime(endereco)
+
+    testaFuncionarios()
 }
 
 fun imprime(valor: Any) : Any{
     println(valor)
     return valor
-} 
+}
 
 
 
