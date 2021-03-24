04fb1101415eedc619bf49d7e13cefc181337792
fabricio-leitao <fabricio-beco@hotmail.com>
2021-03-24 13:28:46 -0300

    Utilizando variaveis globais

--- src/main/kotlin/br/com/zup/bytebank/modelo/Conta.kt
+++ src/main/kotlin/br/com/zup/bytebank/modelo/Conta.kt
@@ -1,11 +1,19 @@
 package br.com.zup.bytebank.modelo
 
+var totalContas = 0
+    private set
+
 abstract class Conta(
     var titular: Cliente,
     val numeroConta: Int = 0
 ) {
     var saldo = 0.0
         protected set
+
+    init {
+        println("Criando conta")
+        totalContas++
+    }
 
     fun deposita(valor: Double) {
         if (valor > 0) {
@@ -22,5 +30,24 @@ abstract class Conta(
             return true
         }
         return false
+    }
+}
+
+class ContaCorrente(
+    titular: Cliente,
+    numeroConta: Int
+) : Conta(
+    titular = titular,
+    numeroConta = numeroConta
+) {
+
+    init {
+        totalContas++
+    }
+    override fun saca(valor: Double) {
+        val valorComtaxa = valor + 0.1
+        if(this.saldo >= valorComtaxa){
+            this.saldo -= valorComtaxa
+        }
     }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,15 @@
 import br.com.zup.bytebank.modelo.Cliente
+import br.com.zup.bytebank.modelo.ContaPoupanca
+import br.com.zup.bytebank.modelo.totalContas
 
 fun main() {
 
+    val deku = Cliente(nome = "Deku", cpf = "", senha = 1)
+    val contaPoupanca = ContaPoupanca(titular = deku, numeroConta = 1000)
+
     testaContasDiferentes()
+
+    println("Total de contas: $totalContas")
 }
 
 
