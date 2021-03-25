01c3097679c139674bd6455d8c62cc235796b016
fabricio-leitao <fabricio-beco@hotmail.com>
2021-03-25 10:58:22 -0300

    Exceptions com o try expression

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,13 @@
+import br.com.zup.bytebank.modelo.Endereco
+import java.lang.ClassCastException
 
 fun main() {
     println("início main")
+    try {
+        10/0
+    } catch (e: ArithmeticException){
+        println("ArithmeticException foi pegada.")
+    }
     funcao1()
     println("fim main")
 
@@ -14,9 +21,26 @@ fun funcao1() {
 
 fun funcao2() {
     println("início funcao2")
-    for (i in 1..5){
-        println(i)
+    try {
+        for (i in 1..5) {
+            println(i)
+            val endereco = Any()
+            endereco as Endereco
+        }
+    }catch (e: ClassCastException){
+        println("ClassCastException foi pega")
     }
+
+//    for (i in 1..5){
+//        println(i)
+//        val endereco = Any()
+//        try {
+//            endereco as Endereco
+//        } catch (e: ClassCastException){
+//            println("ClassCastException foi pega")
+//        }
+//
+//    }
     println("fim funcao2")
 }
 
