111beb2dd3cfd54f41bf42a53f3ead28c7e9b0eb
fabricio-leitao <fabricio-beco@hotmail.com>
2021-03-25 12:06:55 -0300

    Utilizando as expressões

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,43 @@
 import br.com.zup.bytebank.modelo.Endereco
 import java.lang.ClassCastException
+import java.lang.NumberFormatException
 
 fun main() {
     println("início main")
-    try {
-        10/0
-    } catch (e: ArithmeticException){
-        println("ArithmeticException foi pegada.")
+
+    val entrada: String = "1.0"
+
+    var valorRecebido: Double? = try {
+        entrada.toDouble()
+    }catch (e: NumberFormatException){
+        println("Problema na conversão")
+        e.printStackTrace()
+        null
     }
+
+    val valorComTaxa: Double? = if(valorRecebido != null){
+        valorRecebido + 0.1
+    }else {
+        0.0
+    }
+
+    if(valorComTaxa != null){
+        println("Valor recebido: $valorComTaxa")
+    } else{
+        println("valor inválido")
+    }
+
+
+//    val entrada: String = "1,9"
+//
+//    try {
+//        val valor: Double = entrada.toDouble()
+//        println("Valor recebido: $valor")
+//    }catch (e: NumberFormatException){
+//        println("Problema na conversão")
+//        e.printStackTrace()
+//    }
+
     funcao1()
     println("fim main")
 
@@ -33,10 +63,10 @@ fun funcao2() {
             endereco as Endereco
         }
     }catch (e: ClassCastException){
-        println(e.message)
-        println(e.stackTrace)
-        println(e.cause)
-        println(e.printStackTrace())
+//        println(e.message)
+//        println(e.stackTrace)
+//        println(e.cause)
+//        println(e.printStackTrace())
         println("ClassCastException foi pega")
     }
     println("fim funcao2")
