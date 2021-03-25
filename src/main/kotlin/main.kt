ddfde1d06dedd3ac0c005809e10dc41251c52aa7
fabricio-leitao <fabricio-beco@hotmail.com>
2021-03-25 17:09:50 -0300

    Elvis operator e Safe cast

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,21 @@
 import br.com.zup.bytebank.modelo.Endereco
+import java.lang.IllegalStateException
 
 fun main() {
-    var endereco: Endereco? = Endereco(logradouro = "rua vergueiro")
+    var endereco: Endereco? = Endereco(logradouro = "rua vergueiro", complemento = "prédio")
     val logradouroNovo: String? = endereco?.logradouro
-    println(endereco?.logradouro?.length)
-    endereco = null
-    endereco?.let {endereco: Endereco? ->
+    endereco?.let {
         println(endereco?.logradouro?.length)
+        val tamanhoComplemento: Int = it.complemento?.length ?: throw IllegalStateException("Complemento não pode ser vazio")
+        println(tamanhoComplemento)
     }
+
+    teste("")
+    teste(1)
+}
+
+fun teste(valor: Any) {
+    val numero: Int? = valor as? Int
+    println(numero)
 }
 
