55fdb95bceffdeabb650381f9727655a59a567ba
fabricio-leitao <fabricio-beco@hotmail.com>
2021-03-26 18:13:43 -0300

    Imprimindo listas ordenadas

--- src/main/kotlin/br/com/zup/bytebank/list/ListaDeLivros.kt
+++ src/main/kotlin/br/com/zup/bytebank/list/ListaDeLivros.kt
@@ -46,9 +46,17 @@ fun main(){
     println(livros)
 
     livros.imprimeComMarcadores()
+
+    val ordenadoAnoPublicacao: List<Livro> = livros.sorted()
+    ordenadoAnoPublicacao.imprimeComMarcadores()
+
+    val ordenadoPorTitulo = livros.sortedBy { it.titulo }
+    ordenadoPorTitulo.imprimeComMarcadores()
+
+    livros.sortedBy { it.autor }.imprimeComMarcadores()
 }
 
-fun MutableList<Livro>.imprimeComMarcadores(){
+fun List<Livro>.imprimeComMarcadores(){
     val textoFormatado = this.joinToString(separator = "\n") {
         " - ${it.titulo} de ${it.autor}"
     }
