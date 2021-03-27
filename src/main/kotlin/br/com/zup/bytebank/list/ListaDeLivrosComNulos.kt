faaab130ec02da896c2c30f1b3bc81abc46e4851
fabricio-leitao <fabricio-beco@hotmail.com>
2021-03-27 09:37:52 -0300

    Lidando com Nulos

---
+++ src/main/kotlin/br/com/zup/bytebank/list/ListaDeLivrosComNulos.kt
@@ -0,0 +1,6 @@
+package br.com.zup.bytebank.list
+
+fun main() {
+
+    listaLivrosComNulos.imprimeComMarcadores()
+}
--- src/main/kotlin/br/com/zup/bytebank/list/livros.kt
+++ src/main/kotlin/br/com/zup/bytebank/list/livros.kt
@@ -2,7 +2,7 @@ package br.com.zup.bytebank.list
 
 import br.com.zup.bytebank.modelo.Livro
 
-val listaDeLivros = mutableListOf(
+val listaDeLivros: MutableList<Livro> = mutableListOf(
     Livro(
         titulo = "Grande Sertão: Veredas",
         autor = "João Guimarães Rosa",
@@ -19,6 +19,46 @@ val listaDeLivros = mutableListOf(
         autor = "Machado de Assis",
         anoPublicacao = 1881
     ),
+    Livro(
+        titulo = "Iracema",
+        autor = "José de Alencar",
+        anoPublicacao = 1865,
+        editora = "Editora B"
+    ),
+    Livro(
+        titulo = "Mayombe",
+        autor = "Pepetela",
+        anoPublicacao = 1979,
+        editora = "Editora B"
+    ),
+    Livro(
+        titulo = "O Cortiço",
+        autor = "Aluísio Azevedo",
+        anoPublicacao = 1890,
+        editora = "Editora B"
+    )
+)
+
+val listaLivrosComNulos: MutableList<Livro?> = mutableListOf(
+    null,
+    Livro(
+        titulo = "Grande Sertão: Veredas",
+        autor = "João Guimarães Rosa",
+        anoPublicacao = 1956
+    ),
+    null,
+    Livro(
+        titulo = "Minha vida de menina",
+        autor = "Helena Morley",
+        anoPublicacao = 1942,
+        editora = "Editora A"
+    ),
+    Livro(
+        titulo = "Memórias Póstumas de Brás Cubas",
+        autor = "Machado de Assis",
+        anoPublicacao = 1881
+    ),
+    null,
     Livro(
         titulo = "Iracema",
         autor = "José de Alencar",
