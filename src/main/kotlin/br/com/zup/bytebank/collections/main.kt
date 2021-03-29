b72c1a3bbd05a195fd6c7c82b32dcecb5100cd9e
fabricio-leitao <fabricio-beco@hotmail.com>
2021-03-29 10:18:11 -0300

    Comportamentos do Set

--- src/main/kotlin/br/com/zup/bytebank/collections/main.kt
+++ src/main/kotlin/br/com/zup/bytebank/collections/main.kt
@@ -4,13 +4,26 @@ fun main() {
 
     val assistiramCursoAndroid: MutableSet<String> = mutableSetOf("Alex", "Fran", "Gui", "Maria")
     val assistiramCursoKotlin: MutableSet<String> = mutableSetOf("Alex", "Paulo", "Maria")
-//    val assistiramAmbos: Set<String> = assistiramCursoKotlin.plus(assistiramCursoAndroid)
     val assistiramAmbos = mutableSetOf<String>()
     assistiramAmbos.add("Ana")
     assistiramAmbos.add("Ana")
     assistiramAmbos.addAll(assistiramCursoAndroid)
     assistiramAmbos.addAll(assistiramCursoKotlin)
     println(assistiramAmbos)
+
+    println(assistiramCursoAndroid + assistiramCursoKotlin)
+    println(assistiramCursoAndroid.union(assistiramCursoKotlin))
+    //característica unfix que não necessita do ponto
+    println(assistiramCursoAndroid union assistiramCursoKotlin)
+    println(assistiramCursoKotlin - assistiramCursoAndroid)
+    println(assistiramCursoKotlin subtract  assistiramCursoAndroid)
+
+    println(assistiramCursoKotlin intersect assistiramCursoAndroid)
+
+    val assistiramList = assistiramAmbos.toMutableList()
+    assistiramList.add("Ana")
+    println(assistiramList)
+    println(assistiramList.toSet())
 
 }
 
