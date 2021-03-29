5455dfb5de28da1d0c9e23a403825d733f09b306
fabricio-leitao <fabricio-beco@hotmail.com>
2021-03-29 12:08:48 -0300

    testando comportamentos de escrita map

--- src/main/kotlin/br/com/zup/bytebank/collections/main.kt
+++ src/main/kotlin/br/com/zup/bytebank/collections/main.kt
@@ -2,14 +2,41 @@ package br.com.zup.bytebank.collections
 
 fun main() {
 
-    val pedidos = mapOf<Int, Double>(Pair(1, 20.0), Pair(2, 34.0), 3 to 50.0)
+    val pedidos: MutableMap<Int, Double> = mutableMapOf(
+        Pair(1, 20.0),
+        Pair(2, 34.0),
+        3 to 50.0
+    )
     println(pedidos)
     val pedido = pedidos[3]
     pedido?.let {
         println("pedido $it")
     }
-    for (pedido: Map.Entry<Int, Double> in pedidos){
-        println("Número do pedido: ${pedido.key}")
-        println("Valor do pedido: ${pedido.value}")
+    for (p: Map.Entry<Int, Double> in pedidos){
+        println("Número do pedido: ${p.key}")
+        println("Valor do pedido: ${p.value}")
     }
+
+    pedidos[4] = 70.0
+    println(pedidos)
+
+    pedidos.put(5, 80.0)
+    println(pedidos)
+
+    pedidos[1] = 100.0
+    println(pedidos)
+
+    pedidos.putIfAbsent(6, 200.0)
+    println(pedidos)
+
+    //Apenas insere se não existir essa chave
+    pedidos.putIfAbsent(6, 300.0)
+    println(pedidos)
+
+    pedidos.remove(6)
+    println(pedidos)
+
+    //Somente remove se os valores são iguais
+    pedidos.remove(3, 100)
+    println(pedidos)
 }
