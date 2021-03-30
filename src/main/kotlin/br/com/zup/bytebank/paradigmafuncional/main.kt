7db42d97b8d2c22f933c262304c4e3069ee8f8ba
fabricio-leitao <fabricio-beco@hotmail.com>
2021-03-30 13:04:22 -0300

    Explorando implementações com let

--- src/main/kotlin/br/com/zup/bytebank/paradigmafuncional/main.kt
+++ src/main/kotlin/br/com/zup/bytebank/paradigmafuncional/main.kt
@@ -4,13 +4,19 @@ import br.com.zup.bytebank.modelo.Endereco
 
 
 fun main() {
-    val testeFuncao: () -> Unit
-    Endereco().let { it }
-    //higher order function
-    "".let(::testeRecebeString)
-    1.let { it }
-}
+//    val endereco = Endereco(logradouro = "rua vergueiro", numero = 3185)
+//    val enderecoEmMaiusculo = "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
+//    println(enderecoEmMaiusculo)
+    Endereco(logradouro = "rua vergueiro", numero = 3185).let { endereco ->
+        "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
+    }.let(::println)
+//    }    }.let { enderecoEmMaiusculo ->
+//        println(enderecoEmMaiusculo)
+//    }
 
-fun testeRecebeString(valor: String){
-
+    listOf(
+        Endereco(complemento = "casa"),
+        Endereco(),
+        Endereco(complemento = "apartamento")
+    ).filter { endereco -> endereco.complemento.isNotEmpty() }.let(::println)
 }
